66a4e82e7f4e2c81ca23a6db08813ced35737b22
GalinGamboa <galin_71@hotmail.com>
2022-04-02 18:29:45 +0200

    Studi Evaluation 2.1 Version 2

---
+++ js/main.js
@@ -0,0 +1,15 @@
+const newGame = document.getElementById("new_game")
+console.log(newGame)
+const roll_dice = document.getElementById('roll_dice');
+console.log(roll_dice)
+const hold = document.getElementById('hold');
+console.log(hold)
+const points_temp1 = document.getElementById('points_temp1');
+console.log(points_temp1)
+const points_temp2 = document.getElementById('points_temp2');
+console.log(points_temp2)
+const points_glob1 = document.getElementById('points_glob1');
+console.log(points_glob1)
+const points_glob2 = document.getElementById('points_glob2');
+console.log(points_glob2)
+const gagnant = document.querySelector('.gagnant')
