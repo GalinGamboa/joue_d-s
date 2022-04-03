9d15e4ce44ac859f1e3938f31cf378a835562a82
GalinGamboa <galin_71@hotmail.com>
2022-04-03 07:34:14 +0200

    Studi Evaluation 2.1 Version 3

--- js/main.js
+++ js/main.js
@@ -1,15 +1,35 @@
-const newGame = document.getElementById("new_game")
-console.log(newGame)
+const newGame = document.getElementById("new_game");
+console.log(newGame);
 const roll_dice = document.getElementById('roll_dice');
-console.log(roll_dice)
+console.log(roll_dice);
 const hold = document.getElementById('hold');
-console.log(hold)
+console.log(hold);
 const points_temp1 = document.getElementById('points_temp1');
-console.log(points_temp1)
+console.log(points_temp1);
 const points_temp2 = document.getElementById('points_temp2');
-console.log(points_temp2)
+console.log(points_temp2);
 const points_glob1 = document.getElementById('points_glob1');
-console.log(points_glob1)
+console.log(points_glob1);
 const points_glob2 = document.getElementById('points_glob2');
-console.log(points_glob2)
-const gagnant = document.querySelector('.gagnant')
+console.log(points_glob2);
+const gagnant = document.querySelector('.gagnant');
+
+const container_buttons = document.querySelector('.container_buttons')
+console.log(container_buttons)
+
+newGame.addEventListener('click',()=>{
+    
+    a = 0;
+    b = 0;
+    c = 0;
+    d = 0;
+    points_glob1.textContent='0'
+    points_glob2.textContent='0'
+    points_temp1.textContent='0'
+    points_temp2.textContent='0'
+    gagnant.textContent=('')
+    container_buttons.style.display = ('block')
+    document.querySelector('.dice_1').setAttribute('src', "/images/img/dé1.jpg")
+    
+})
+
