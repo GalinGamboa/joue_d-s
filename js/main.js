89d91b294e1b0bf4983c764b8334bf4becc716da
GalinGamboa <galin_71@hotmail.com>
2022-04-03 15:09:38 +0200

    Studi Evaluation 2.1 version 5

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ const points_temp1 = document.getElementById('points_temp1');
 const points_temp2 = document.getElementById('points_temp2');
 
 const points_glob1 = document.getElementById('points_glob1');
-;
+
 const points_glob2 = document.getElementById('points_glob2');
 
 const gagnant = document.querySelector('.gagnant');
@@ -30,21 +30,21 @@ newGame.addEventListener('click',()=>{
     gagnant.textContent=('');
     container_buttons.style.display = ('block');
     document.querySelector('.dice_1').setAttribute('src', "/images/img/dé1.jpg");
+    document.querySelector('.container_player1').style.border = ('solid 4px rgb(117, 8, 190)')
+    document.querySelector('.container_player2').style.border = ('')
+    if (points_temp1.classList.contains('player2')==true) {
+        points_temp1.classList.remove('player2')
+    }
 })
-
 
 game()
 
 function game (){
 
-
     a = 0;
     b = 0;
     c = 0;
     d = 0;
-
-    
-
     
     function changer (){
    
@@ -52,13 +52,17 @@ function game (){
     
             if (points_temp1.classList.contains('player2')==false){
                 points_temp1.classList.add('player2')
+                document.querySelector('.container_player2').style.border = ('solid 4px rgb(117, 8, 190)')
+                document.querySelector('.container_player1').style.border = ('')
                
                 c = c+a
                 points_glob1.textContent=c
 
-                if (c>=30){
-                    gagnant.textContent=('GAGNANT JOUER 1')
+                if (c>=100){
+                    gagnant.textContent=('GAGNANT  PLAYER  1')
                     container_buttons.style.display = ('none');
+                    document.querySelector('.container_player2').style.border = ('')
+                    document.querySelector('.container_player1').style.border = ('solid 4px red')
                    
                 }
                 save()
@@ -66,12 +70,16 @@ function game (){
     
             else    {
                 points_temp1.classList.remove('player2')
+                document.querySelector('.container_player1').style.border = ('solid 4px rgb(117, 8, 190)')
+                document.querySelector('.container_player2').style.border = ('')
               
                 d = d +b
                 points_glob2.textContent=d
-                    if(d>=30){
-                        gagnant.textContent=('GAGNANT JOUER 2')
+                    if(d>=100){
+                        gagnant.textContent=('GAGNANT  PLAYER  2')
                         container_buttons.style.display = ('none');
+                        document.querySelector('.container_player2').style.border = ('solid 4px red')
+                        document.querySelector('.container_player1').style.border = ('')
                        
                     }
                 save()
@@ -85,44 +93,37 @@ function game (){
         b = 0;
         points_temp1.textContent=a
         points_temp2.textContent=b
-
     }
 
     changer()
 
     roll_dice.addEventListener('click', function(){
  
-        console.log(points_temp1.classList.contains('player2'))
+        //  console.log(points_temp1.classList.contains('player2'))
         if (points_temp1.classList.contains('player2')==false){
-
-            
+               
             var dice1 = Math.floor(Math.random() * 6 + 1);
             document.querySelector('.dice_1').setAttribute('src', `/images/img/dé${dice1}.jpg`)
-            console.log(dice1)
-           
-
-
+            //  console.log(dice1)
+            
             if (dice1!==1){
                 a=dice1+a
                 points_temp1.textContent =a
             }
+
             else if (dice1==1) {
-               
+                document.querySelector('.container_player2').style.border = ('solid 4px rgb(117, 8, 190)')
+                document.querySelector('.container_player1').style.border = ('')
                 points_temp1.classList.add('player2')
                
                 save() 
             }
-                
-                
-            
-            
         }
-       
+
         else {
-           
             var dice1 = Math.floor(Math.random() * 6 + 1);
             document.querySelector('.dice_1').setAttribute('src', `/images/img/dé${dice1}.jpg`)
-            console.log(dice1)
+            //  console.log(dice1)
            
             if (dice1!==1){
                 b=dice1+b
@@ -131,16 +132,11 @@ function game (){
             else if (dice1==1) {
               
                 points_temp1.classList.remove('player2')
+                document.querySelector('.container_player1').style.border = ('solid 4px rgb(117, 8, 190)')
+                document.querySelector('.container_player2').style.border = ('')
                
                 save() 
             }
-           
-           
-            
         }
-   
-        
     })
-
-
 }
